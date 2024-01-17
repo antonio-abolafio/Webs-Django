5a86a923ff9a7514e15a36ab8a65d9a56d331870
abolafiolopez <abolafiolopez@gmail.com>
2024-01-17 17:54:27 +0100

    url upload media corregida

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name='Título')
     description = models.TextField(verbose_name='Descripcción')
-    image = models.ImageField(verbose_name='Imagen', upload_to='projectsmo')
+    image = models.ImageField(verbose_name='Imagen', upload_to='projects')
     link = models.URLField(null=True, blank=True, verbose_name='Dirección web')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
