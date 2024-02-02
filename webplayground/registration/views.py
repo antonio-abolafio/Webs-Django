7c9db80e6575e1358ac7f7fab6de335d5c5b7a11
abolafiolopez <abolafiolopez@gmail.com>
2024-02-02 11:47:24 +0100

    Método get_form para modificar la vista del formulario de registro y nav Registro en base si no se está logueado

--- webplayground/registration/views.py
+++ webplayground/registration/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
+from django import forms
 
 
 # Create your views here.
@@ -10,3 +11,20 @@ class SignUpView(CreateView):
 
     def get_success_url(self):
         return reverse_lazy('login') + '?register'
+
+    def get_form(self, form_class=None):
+        form = super(SignUpView, self).get_form()
+        # Modificar widgets
+        form.fields['username'].widget = forms.TextInput(attrs={
+            'placeholder': 'Nombre de usuario',
+            'class': 'form-control',
+        })
+        form.fields['password1'].widget = forms.PasswordInput(attrs={
+            'placeholder': 'Contraseña',
+            'class': 'form-control',
+        })
+        form.fields['password2'].widget = forms.PasswordInput(attrs={
+            'placeholder': 'Repite la contraseña',
+            'class': 'form-control',
+        })
+        return form
