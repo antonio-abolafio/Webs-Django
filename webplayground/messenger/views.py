55730dd9c3e21d785e9d19c2b74ca2d26fa48113
abolafiolopez <abolafiolopez@gmail.com>
2024-02-08 19:07:20 +0100

    Mensajes asíncronos con javascript para la app messenger. Añadido el text area y botón de prueba para hacer los envíos

--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -3,9 +3,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
-from messenger.models import Thread
+from messenger.models import Thread, Message
+from django.shortcuts import get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -31,3 +33,17 @@ class ThreadDetail(DetailView):
         if self.request.user not in obj.users.all():
             raise Http404
         return obj
+
+
+def add_message(request, pk):
+    json_response = {'created': False}
+    if request.user.is_authenticated:
+        content = request.GET.get('message', None)
+        if content:
+            thread = get_object_or_404(Thread, pk=pk)
+            message = Message.objects.create(user=request.user, message=content, thread=thread)
+            thread.messages.add(message)
+            json_response['created'] = True
+    else:
+        raise Http404("User is not authenticated")
+    return JsonResponse(json_response)
