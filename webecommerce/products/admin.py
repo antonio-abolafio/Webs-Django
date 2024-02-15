d465ac7e9bc5be686dfb116b4c70b76ef4730a8f
abolafiolopez <abolafiolopez@gmail.com>
2024-02-15 19:48:06 +0100

    Modelo de etiquetas creado. Categorías, etiqutas y productos añadidos al panel de administración

--- webecommerce/products/admin.py
+++ webecommerce/products/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Product, Category, Tag
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class TagAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class ProductAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+    list_display = ('name', 'price', 'product_category', 'description', 'product_tag', 'image')
+    ordering = ('name', 'categories')
+    search_fields = ('name', 'categories', 'tags', 'price')
+    date_hierarchy = 'updated'
+    list_filter = ('categories', 'tags')
+
+    @staticmethod
+    def product_category(obj):
+        return ", ".join(category.name for category in obj.categories.all().order_by('name'))
+
+    @staticmethod
+    def product_tag(obj):
+        return ", ".join(tag.name for tag in obj.tags.all().order_by('name'))
+
+
+admin.site.register(Tag, TagAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
--- webecommerce/products/models.py
+++ webecommerce/products/models.py
@@ -15,13 +15,27 @@ class Category(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Nombre')
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
+    class Meta:
+        verbose_name = "Etiqueta"
+        verbose_name_plural = "Etiquetas"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
     image = models.ImageField(upload_to='media/products')
-    sku = models.SlugField(unique=True)
     categories = models.ManyToManyField(Category, verbose_name='Categoría')
+    tags = models.ManyToManyField(Tag, verbose_name='Etiquetas')
     created = models.DateTimeField(auto_now_add=True, verbose_name='Fecha de creación')
     updated = models.DateTimeField(auto_now=True, verbose_name='Fecha de edición')
 
