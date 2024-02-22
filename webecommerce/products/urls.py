63ae0e9693fb831f4720d8afd4b236203f230185
abolafiolopez <abolafiolopez@gmail.com>
2024-02-22 14:12:08 +0100

    template y vista de categorías, para filtrar los productos por categorías

--- webecommerce/products/urls.py
+++ webecommerce/products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, BrandListView
 
 products_patterns = ([
     path('', ProductListView.as_view(), name='list'),
-    path('<str:name>/', ProductDetailView.as_view(), name='detail')
+    path('brand/<str:brand>/', BrandListView.as_view(), name='brand'),
+    path('product/<str:name>/', ProductDetailView.as_view(), name='detail'),
 ], "products")
--- webecommerce/products/views.py
+++ webecommerce/products/views.py
@@ -7,13 +7,24 @@ from .models import Product
 class ProductListView(ListView):
     model = Product
     template_name = "products/shop.html"
+    context_object_name = "products"
 
 
 class ProductDetailView(DetailView):
     model = Product
     template_name = "products/detail.html"
+    context_object_name = "product"
 
     # Buscar el producto por el nombre
     def get_object(self, queryset=None):
         name = self.kwargs.get("name")
-        return get_object_or_404(Product, name=name)
+        return Product.objects.get(name=name)
+
+
+class BrandListView(ListView):
+    model = Product
+    template_name = "products/category.html"
+
+    def get_queryset(self):
+        brand_name = self.kwargs.get("brand")
+        return Product.objects.filter(brand__name=brand_name)
